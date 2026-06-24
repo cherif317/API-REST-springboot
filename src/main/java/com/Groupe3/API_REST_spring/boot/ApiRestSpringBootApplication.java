f127a0992f307fed5ec5292007eb68dfda883994
Cherif Sarr <cherifsarr317@gmail.com>
2026-06-24 19:11:08 +0000

    premier commit

---
+++ src/main/java/com/Groupe3/API_REST_spring/boot/ApiRestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.Groupe3.API_REST_spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSpringBootApplication.class, args);
+	}
+
+}
