a56566c3a215e5c3348d7bd7d6b80c74223445f8
Cherif Sarr <cherifsarr317@gmail.com>
2026-07-09 05:10:35 +0000

    Structure du projet

---
+++ src/main/java/com/Groupe3/API_REST_spring/boot/dto/etudiant/EtudiantDTO.java
@@ -0,0 +1,4 @@
+package com.Groupe3.API_REST_spring.boot.dto.etudiant;
+
+public class EtudiantDTO {
+}
---
+++ src/main/java/com/Groupe3/API_REST_spring/boot/enums/RoleName.java
@@ -0,0 +1,4 @@
+package com.Groupe3.API_REST_spring.boot.enums;
+
+public class RoleName {
+}
